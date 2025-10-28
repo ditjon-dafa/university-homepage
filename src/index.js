f510b74a44b021e6dc89036ac111f8e478762ac6
Ditjon <dafaditjon@gmail.com>
2025-10-28 12:28:54 +0100

    Fixing Carousel design issue. Toggle menu show/hide page content in small screen.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 let isShownMenuContent = false;
 
 const TOGGLE_MENU = document.getElementById("toggle-menu");
-TOGGLE_MENU.addEventListener("click", toggleSecondNavBar);
+TOGGLE_MENU.addEventListener("click", toggleMenu);
 
-function toggleSecondNavBar() {
+function toggleMenu() {
   isShownMenuContent = !isShownMenuContent;
-  showHideSecondNavBar();
+  showHideMenuContent();
 }
 
-function showHideSecondNavBar() {
+function showHideMenuContent() {
   const SECOND_NAV_BAR_CONTAINER = document.getElementById("second-nav-bar-lg");
 
   SECOND_NAV_BAR_CONTAINER.classList.toggle(
     "hide",
     isShownMenuContent === true
   );
+
+  const UNIVERSITY_CONTENT_SMALL_SCREEN = document.querySelectorAll(
+    ".university-content-small-screen"
+  );
+  UNIVERSITY_CONTENT_SMALL_SCREEN.forEach((content) => {
+    content.classList.toggle("hide", isShownMenuContent === true);
+  });
 }
