350e1667b8f4af864a1a14b6ea04b8bed0603628
Ditjon <dafaditjon@gmail.com>
2025-11-02 09:15:34 +0100

    The page content is hidden while the menu content is shown in the small screen. Otherwise, the menu content has focus over the underlying content. If the menu content is shown and the browser window is not maximazed, the menu content is collapsed when the browser maximazed button is clicked.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 let isShownMenuContent = false;
 
+let firstDocumentBodyWidth = 0;
+let previousWindowWidth = 0;
+let currentWindowWidth = 0;
+
 const TOGGLE_MENU = document.getElementById("toggle-menu");
 TOGGLE_MENU.addEventListener("click", toggleMenu);
 
+function initialUpdate() {
+  firstDocumentBodyWidth = screen.availWidth;
+  previousWindowWidth = firstDocumentBodyWidth;
+  currentWindowWidth = 0;
+  updateSize();
+}
+
 function toggleMenu() {
   isShownMenuContent = !isShownMenuContent;
+  updateSize();
   showHideMenuContent();
 }
 
@@ -15,11 +27,42 @@ function showHideMenuContent() {
     "hide",
     isShownMenuContent === true
   );
+}
+
+//function to update Window Size (width)
+function updateSize() {
+  currentWindowWidth = window.innerWidth;
 
   const UNIVERSITY_CONTENT_SMALL_SCREEN = document.querySelectorAll(
     ".university-content-small-screen"
   );
-  UNIVERSITY_CONTENT_SMALL_SCREEN.forEach((content) => {
-    content.classList.toggle("hide", isShownMenuContent === true);
-  });
+  if (currentWindowWidth <= 767) {
+    UNIVERSITY_CONTENT_SMALL_SCREEN.forEach((content) => {
+      content.classList.toggle("hide", isShownMenuContent === true);
+    });
+  } else {
+    UNIVERSITY_CONTENT_SMALL_SCREEN.forEach((content) => {
+      if (isShownMenuContent === true && content.classList.contains("hide"))
+        content.classList.remove("hide");
+    });
+  }
+  if (
+    previousWindowWidth < firstDocumentBodyWidth &&
+    currentWindowWidth === firstDocumentBodyWidth
+  ) {
+    isShownMenuContent = !isShownMenuContent;
+    showHideMenuContent();
+
+    const collapseElementList = document.querySelectorAll(".collapse");
+    const collapseList = [...collapseElementList].map(
+      (collapseEl) => new bootstrap.Collapse(collapseEl)
+    );
+  }
+  previousWindowWidth = currentWindowWidth;
 }
+
+//add event listener for window resize
+window.addEventListener("resize", updateSize);
+
+//initial update of browser screen width
+initialUpdate();
